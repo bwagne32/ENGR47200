903b88151a4950eba29150dd2808939019d1490f
bwagne32 <bwagne32@kent.edu>
2023-10-26 10:32:59 -0400

    implemented tictactoe

--- header.py
+++ header.py
@@ -4,8 +4,8 @@ import time
 
 class battleShip:
     def __init__(self, size: int):
-        self._size: int = size
-        self._board = [['O'] * size for _ in range(size)]
+        self.size: int = size
+        self.board = [['O'] * size for _ in range(size)]
         for i in range(size):
             randRow = random.randrange(size)
             randCol = random.randrange(size)
@@ -13,37 +13,117 @@ class battleShip:
                 self._board[randRow][randCol] = 'X'
             else:
                 i -= i
-        self._ships = size
+        self.ships = size
             
     def check(self, row: int, column: int):
-        return self._board[row][column] == 'O'
+        return self.board[row][column] == 'O'
     
     def printBoard(self): #prints rows x columns for debugging/cheating
-        for i in self._board:
+        for i in self.board:
             print(" " + str(i))
-        print("Ships left: " + str(self._ships))
+        print("Ships left: " + str(self.ships))
     
     def fire(self, row: int, col: int):
         if(self.check(row,col)):
             print("Miss")
         else:
             print("Hit")
-            self._board[row][col] = 'O'
-            self._ships -= 1
+            self.board[row][col] = 'O'
+            self.ships -= 1
 
-    def game(self):
+    def play(self):
         while(self._ships > 0):
-            print("Ships left: " + str(self._ships))
+            print("Ships left: " + str(self.ships))
             self.printBoard()
-            row: int = input("Input row to fire at: (1 - " + str(self._size) + "): ")
-            col: int = input("Input column to fire at: (1 - " + str(self._size) + "): ")
+            row: int = input("Input row to fire at: (1 - " + str(self.size) + "): ")
+            col: int = input("Input column to fire at: (1 - " + str(self.size) + "): ")
             row: int = int(row)
             col: int = int(col)
-            if((int(row) > int(self._size)) or (int(col) > int(self._size))):
+            if((int(row) > int(self.size)) or (int(col) > int(self.size))):
                 print("Out of range")
                 continue
-            #try:
+        
             self.fire(row - 1, col - 1)
-            #except:
-            #    print("Only input integers less than " + str(self._size + 1))
         print("You win!")
+
+
+
+class connectFour:
+    def __init__(self):
+        self.board = [['*'] * 7 for _ in range(6)]
+
+    def printBoard(self):
+        for i in range(6):
+            print(self.board[i])
+
+    def winCheck(self):
+        directions = [(0, 1), (1, 0), (1, 1), (-1, 1)]
+        for row in range(self.rows):
+            for col in range(self.columns):
+                if self.board[row][col] == " ":
+                    continue
+                for dr, dc in directions:
+                    for _ in range(4):
+                        r, c = row + dr * _, col + dc * _
+                        if 0 <= r < self.rows and 0 <= c < self.columns and self.board[r][c] == self.board[row][col]:
+                            continue
+                        break
+                    else:
+                        return self.board[row][col]
+        if self.moves == self.rows * self.columns:
+            return "Tie"
+        return None
+
+class TicTacToe:
+    def __init__(self):
+        self.board = [" " for _ in range(9)]  # Create an empty board
+        self.current_player = "X"
+
+    def print_board(self):
+        for i in range(0, 9, 3):
+            print(" | ".join(self.board[i:i + 3]))
+            if i < 6:
+                print("-" * 9)
+
+    def make_move(self, position):
+        if self.board[position] == " ":
+            self.board[position] = self.current_player
+            self.current_player = "X" if self.current_player == "O" else "O"
+            return True
+        else:
+            return False
+
+    def check_winner(self):
+        winning_combinations = [
+            (0, 1, 2), (3, 4, 5), (6, 7, 8),
+            (0, 3, 6), (1, 4, 7), (2, 5, 8),
+            (0, 4, 8), (2, 4, 6)
+        ]
+
+        for combo in winning_combinations:
+            if self.board[combo[0]] == self.board[combo[1]] == self.board[combo[2]] != " ":
+                return self.board[combo[0]]
+        if " " not in self.board:
+            return "Tie"
+        return None
+
+    def play(self):
+        while True:
+            self.print_board()
+            try:
+                position = int(input(f"Player {self.current_player}, enter your move (1-9): "))
+                if position < 1 or position > 9:
+                    raise ValueError("Position out of range.")
+            except (ValueError, IndexError):
+                print("Invalid input. Try again.")
+                continue
+
+            if self.make_move(position - 1):
+                winner = self.check_winner()
+                if winner:
+                    self.print_board()
+                    if winner == "Tie":
+                        print("It's a tie!")
+                    else:
+                        print(f"Player {winner} wins!")
+                    break
--- mobileApp.py
+++ mobileApp.py
@@ -7,12 +7,18 @@ import random
 #random.seed(time.time())
 random.seed(1) #debugging
 
-game = 0
-while(game != 4):
-    game = int(input("What game would you like to play (1-4): \n1. BattleShip\n2. Connect Four\n3. Tic Tac Toe\n4. Exit\n"))
-    if(game == 1):
+choice = 0
+while(choice != 4):
+    choice = int(input("What game would you like to play (1-4): \n1. BattleShip\n2. Connect Four\n3. Tic Tac Toe (2 player)\n4. Exit\n"))
+    if(choice == 1):
         size = input("Board size")
-        ship = battleShip(size)
-        ship.game()
-    elif(game == 4):
-        break
+        game = battleShip(size)
+        game.play()
+    if(choice == 2):
+        game = connectFour()
+        game.printBoard()
+    if(choice == 3):
+        game = TicTacToe()
+        game.play()
+    elif(choice == 4):
+        continue
