d041c47b69ef7b0d374edeba46632d57f6a356dd
bwagne32 <bwagne32@kent.edu>
2023-12-06 11:12:43 -0500

    Changed game seed to be random

--- mobileApp.py
+++ mobileApp.py
@@ -3,21 +3,19 @@ from header import *
 import time
 import random
 
+random.seed()
+#random.seed(1) #debugging
 
-#random.seed(time.time())
-random.seed(1) #debugging
 username = login()
-
+print("Hi, " + username + "\n")
 
 choice = 0
 while(choice != 4):
-    choice = int(input("Hi, " + username + "\n"
-                       "1. BattleShip (1 player)\n"
+    choice = int(input("1. BattleShip (1 player)\n"
                        "2. Connect Four (Cut from final project)\n"
                        "3. Tic Tac Toe\n"
                        "4. Exit\n"
-                       + username + 
-                       ", what game would you like to play (1-4): \0"))
+                       + username + ", what game would you like to play (1-4): \0"))
     if(choice == 1):
         size = int(input("Board size: "))
         game = battleShip(size)
